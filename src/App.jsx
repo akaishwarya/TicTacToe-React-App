63186bbba1c5e1003c3dd56ca82959db7f156191
akaishwarya <akaishwarya88@gmail.com>
2024-12-29 20:43:00 +0530

    Initial Uload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Tictactoe } from './Components/Tic-Tac-Toe/Tictactoe'
+
+function App() {
+  
+
+  return (
+    <>
+    <Tictactoe/>
+    </>
+  )
+}
+
+export default App
